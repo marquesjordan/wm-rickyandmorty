cdcb8020fd0aab4c6f2b17b6f1e0eeb815134c58
marquesjordan <marques.jordan81@gmail.com>
2023-11-14 11:43:20 -0800

    Add ReadMe and Header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import '../app/globals.css';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ShowProvider>
+      <div style={{ textAlign: 'center' }}>Rick and Morty Characters</div>
       <Component {...pageProps} />
     </ShowProvider>
   );
